26f16595c9248ae64d5004f322a7d4a0f68a185d
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-10-23 11:37:29 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get 'fournisseurs', to: 'pages#fournisseurs'
+  get 'furnishers', to: 'pages#fournisseurs'
   get 'stocks', to: 'pages#stocks'
   get 'comptabilite', to: 'pages#comptabilite'
   resources :fournisseurs
