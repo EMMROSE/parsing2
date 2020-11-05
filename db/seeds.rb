f9e9eddc3da2b7059312a67993b7187e05deb237
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-11-05 15:29:49 +0100

    CSS for listing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Destroy all Clothes'
+Clothe.destroy_all
+puts 'Destroy all Brands'
+Brand.destroy_all
+
+array = ['10IS', 'Absorba', 'Alice', 'Arsène et les pipelettes', 'Benetton', 'Billieblush', 'Blune', 'Bonpoint', 'Bonton', 'Boutchou', 'Burberry', 'C&A', 'Carrément Beau', 'Catimini', 'CdeC', 'Chipie', 'Cyrillus', 'Dior', 'DPAM', 'Gap', 'Gocco', 'H&M', 'IKKS', 'Isabel Marant', 'Jacadi', 'Kiabi', 'Kickers', 'K-way', 'La Redoute', 'Le Bome', 'Levis', 'Lola Palacios', 'Louis Louise', 'Mango', 'Monoprix', 'Natalys', 'Noukies', 'Obaidi', 'Okaïdi', 'Orchestra', 'Oscar & Valentine', 'Oshkosh', 'Paul Smith', 'Petit Bateau', 'Ralph Lauren', 'Ronron', 'Sergent Major', 'Stella McCartney', 'TAO', 'Tape à l œil', 'Tartine & Chocolat', 'The Little White Company', 'Timberland', 'Vert Baudet', 'Zadig & Voltaire', 'Zara']
+array2 = ['Bloomer', 'Bloomer Salopette', 'Blouse', 'Body', 'Chaussures', 'Chaussons', 'Chemise', 'Combinaison', 'Doudoune', 'Ensemble', 'Gilet', 'Haut Manches Longues', 'Jupe', 'Manteau', 'Pantalon', 'Pull', 'Pyjama', 'Robe', 'Salopette', 'Veste']
+
+
+puts "CREATING BRANDS"
+puts '----------------'
+array.each do |element|
+  x = Brand.new(name: element)
+  x.save
+end
+puts '>>>>>> Done!'
+puts '----------------'
+
+puts "CREATING CLOTHES"
+puts '----------------'
+array2.each do |element|
+  y = Clothe.new(name: element)
+  y.save
+end
+puts '>>>>>> Done!'
+puts '----------------'
